078e63c23ee5b7d7a488f25ac43b123b1c9509a9
melihabi <melih@uni-bremen.de>
2023-05-26 00:42:36 +0200

    webapps aufgabe1 done

--- webapps/blatt2/Register & Login/script.js
+++ webapps/blatt2/Register & Login/script.js
@@ -12,16 +12,77 @@ function openForm(evt, formName) {
     evt.currentTarget.className += " active";
   }
   
-  document.getElementById("register").addEventListener("submit", function(e) {
-    e.preventDefault();
+  // Passwort-Feld referenzieren
+var passwordInput = document.getElementById("password");
 
+// Event-Listener für das Passwort-Feld hinzufügen
+passwordInput.addEventListener("blur", function() {
+  validatePassword();
+});
 
-    console.log("Registration submitted");
-  });
-  
-  document.getElementById("login").addEventListener("submit", function(e) {
-    e.preventDefault();
+passwordInput.addEventListener("focus", function() {
+  hidePasswordHint();
+});
 
-    
-    console.log("Login submitted");
-  });
+// Funktion zur Überprüfung des Passworts
+function validatePassword() {
+  var password = passwordInput.value;
+  var passwordRepeat = document.getElementById("password-repeat").value;
+  var passwordHint = document.getElementById("password-hint");
+
+  // Hinweis zurücksetzen
+  hidePasswordHint();
+
+  // Mindestlänge von 7 Zeichen überprüfen
+  if (password.length < 7) {
+    passwordHint.textContent = "The password must be at least 7 characters long.";
+    passwordHint.style.display = "block";
+    return;
+  }
+
+  // Mindestens 5 Ziffern überprüfen
+  var digitCount = password.match(/\d/g).length;
+  if (digitCount < 5) {
+    passwordHint.textContent = "The password must contain at least 5 digits.";
+    passwordHint.style.display = "block";
+    return;
+  }
+
+  // Mindestens einen Großbuchstaben überprüfen
+  if (!/[A-Z]/.test(password)) {
+    passwordHint.textContent = "The password must contain at least one capital letter.";
+    passwordHint.style.display = "block";
+    return;
+  }
+
+  // Mindestens einen Kleinbuchstaben überprüfen
+  if (!/[a-z]/.test(password)) {
+    passwordHint.textContent = "The password must contain at least one lowercase letter.";
+    passwordHint.style.display = "block";
+    return;
+  }
+
+  // Passwort-Wiederholung überprüfen
+  if (password !== passwordRepeat) {
+    passwordHint.textContent = "The passwords do not match.";
+    passwordHint.style.display = "block";
+    return;
+  }
+}
+
+// Funktion zum Ausblenden des Hinweises
+function hidePasswordHint() {
+  var passwordHint = document.getElementById("password-hint");
+  passwordHint.style.display = "none";
+}
+
+// Event-Listener für das Formular hinzufügen
+document.getElementById("register").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // Passwort überprüfen
+  validatePassword();
+
+  // Wenn alle Überprüfungen erfolgreich sind, können Sie den Registrierungsprozess fortsetzen
+  console.log("Registration submitted");
+});
